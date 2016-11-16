d4d034b71eb8bed5a2d94e6a825bc8ed3693640f
Walter Reyes <wal.reyes.11@gmail.com>
2016-11-16 12:56:11 -0600

    Adds Artemis::Bot::MessageRequest
    Why is this change necessary?
      Instead of passing the complicated params hash to Artemis::Bot::Conversator(workspace_id, params)
      you can instantiate Artemis::Bot::MessageRequest and pass it instead

---
+++ lib/artemis/bot/message_request.rb
@@ -0,0 +1,18 @@
+require "artemis/bot/input"
+
+module Artemis
+  module Bot
+    class MessageRequest
+      attr_accessor :input, :alternate_intents, :context, :entities, :intents, :output
+
+      def initialize(attrs = {})
+        @input = attrs["input"] || Artemis::Bot::Input.new
+        @alternate_intents = attrs["alternate_intents"]
+        @context = attrs["context"]
+        @entities = attrs["entities"]
+        @intents = attrs["intents"]
+        @output = attrs["output"]
+      end
+    end
+  end
+end
