ddc6c16542ba48197de40873e887935c3b5d3e54
Walter Reyes <wal.reyes.11@gmail.com>
2016-11-16 12:58:12 -0600

    Adds Service ObjectToHash
    Why is this change necessary?
      This class takes an object and converts it to a hash.
      This will be used to convert MessageRequest to a hash and then send it to the api.
    
      The option include_nils needs to be there because if we send the key context with value nil to the watson api it won't work.
      Context and output must have some value in order to work

---
+++ lib/artemis/services/object_to_hash.rb
@@ -0,0 +1,29 @@
+module Artemis
+  module Services
+    module ObjectToHash
+
+      def self.convert(object, opts = {include_nils: true})
+        hash = {}
+        object.instance_variables.each do |var|
+          var_value = object.instance_variable_get(var)
+
+          next if !opts["include_nils"] && var_value.nil?
+
+          hash[var.to_s.delete("@")] = if descendant?(var_value, Artemis::Bot)
+                                         self.convert(var_value)
+                                       else
+                                         hash[var.to_s.delete("@")] = var_value
+                                       end
+        end
+
+        hash
+      end
+
+      def self.descendant?(value, parent)
+        parent = parent.class unless parent.is_a?(Class) || parent.is_a?(Module)
+        value.class.to_s.index("#{parent}::") == 0
+      end
+
+    end
+  end
+end
