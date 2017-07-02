526576f451e0aee001ef421d37d13ae4d7ba7b23
renata-sg <renata.saldivar97@gmail.com>
2017-07-02 12:47:10 -0500

    Adds test coverage

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in artemis-bot.gemspec
 gemspec
 gem 'pry'
+
+group :test do
+  gem "simplecov"
+  gem "codeclimate-test-reporter", "~> 1.0.0"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'artemis/bot'
+require 'artemis/bot/workspace'
+require 'artemis/bot/workspace/workspace_example'
+require 'artemis/bot/workspace/workspace_collection'
+require 'artemis/bot/workspace/workspace_export'
+require "simplecov"
+SimpleCov.start
