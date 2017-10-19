f8be4bb36c8eec46744745468a3d1d3468af6603
Jessica Zahnd <jessica@jessicazahnd.com>
2017-10-19 09:07:13 -0400

    Add confidence to Entity

--- lib/artemis/bot/entity.rb
+++ lib/artemis/bot/entity.rb
@@ -1,12 +1,13 @@
 module Artemis
   module Bot
     class Entity
-      attr_reader :entity, :location, :value
+      attr_reader :entity, :location, :value, :confidence
 
       def initialize(attrs = {})
         @entity = attrs['entity']
         @location = attrs['location']
         @value = attrs['value']
+        @confidence = attrs['confidence']
       end
     end
   end
